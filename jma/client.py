381412cc758576b12f6387bd1b32cebbd3d1c669
Dan Liberatori <daniel.liberatori@gmail.com>
2021-01-13 01:07:25 +0900

    Add new station IDs

--- jma/client.py
+++ jma/client.py
@@ -13,40 +13,57 @@ logger = logging.getLogger('jmaclient')
 
 
 class JmaStation(Enum):
-    Akita =     's47582' # akita
-    Aomori =    's47575' # aomori
-    Fukuoka =   's47807' # fukuoka
-    Hiroshima = 's47765' # hiroshima
-    Kagoshima = 's47827' # kagoshima
-    Kumamoto =  's47819' # kumamoto
-    Matsue =    's47741' # shimane
-    Miyakazki = 's47830' # miyazaki
-    Morioka =   's47584' # iwate
-    Nagasaki =  's47817' # nagasaki
-    Naha =      's47936' # okinawa
-    Oita =      's47815' # oita
-    Saga =      's47813' # saga
-    Sapporo =   's47412' # hokkaido
-    Sendai =    's47590' # miyagi
-    Yamagata =  's47588' # yamagata
+    Akita =      's47582' # akita
+    Aomori =     's47575' # aomori
+    Fukuoka =    's47807' # fukuoka
+    Hikone =     's47761' # shiga
+    Hiroshima =  's47765' # hiroshima
+    Kagoshima =  's47827' # kagoshima
+    Kumamoto =   's47819' # kumamoto
+    Matsue =     's47741' # shimane
+    Miyakazki =  's47830' # miyazaki
+    Morioka =    's47584' # iwate
+    Nagasaki =   's47817' # nagasaki
+    Naha =       's47936' # okinawa
+    Niigata =    's47604' # niigata
+    Oita =       's47815' # oita
+    Saga =       's47813' # saga
+    Sapporo =    's47412' # hokkaido
+    Shizuoka =   's47656' # shizuoka
+    Sendai =     's47590' # miyagi
+    Toyama =     's47607' # toyama
+    Yamagata =   's47588' # yamagata
 
     # These sites don't have irradiation data
-    Aburatsu =  's47835' # okinawa
-    Fukaura =   's47574' # aomori
-    Fukuyama =  's47767' # hiroshima
-    Gifu =      's47632' # gifu
-    Hachinohe = 's47581' # aomori
-    Hamada =    's47755' # shimane
-    Hitoyoshi = 's47824'
+    Aburatsu =   's47835' # okinawa
+    Fuji =       'a0442' # shizuoka
+    Fujisan =    's47639' # shizuoka
+    Fukaura =    's47574' # aomori
+    Fukuyama =   's47767' # hiroshima
+    Gifu =       's47632' # gifu
+    Hachinohe =  's47581' # aomori
+    Hamada =     's47755' # shimane
+    Hamamatsu =  's47654' # shizuoka
+    Himeji =     's47769' # gunma
+    Hitoyoshi =  's47824' # kumamoto
+    Ibukiksan =  's47751' # shiga
     Ishinomaki = 's47592' # miyagi
-    Nago =      's47940' # okinawa
-    Nobeoka =   's47822' # miyazaki
-    Okayama =   's47768' # okayama
-    Sakata =    's47587' # yamagata
-    Takayama =  's47617' # gifu
-    Tottori =   's47746' # tottori
-    Yamaguchi = 's47784' # yamaguchi
-
+    Kobe =       's47770' # gunma
+    Maibara =    'a1524' # shiga
+    Nago =       's47940' # okinawa
+    Nobeoka =    's47822' # miyazaki
+    Okayama =    's47768' # okayama
+    Owase =      's47663' # mie
+    Sakata =     's47587' # yamagata
+    Sumoto =     's47776' # gunma
+    Takayama =   's47617' # gifu
+    Tottori =    's47746' # tottori
+    Toyooka =    's47747' # gunma
+    Tsu =        's47651' # mie
+    Tsuyama =    's47756' # okayama
+    Ueno =       's47649' # mie
+    Yamaguchi =  's47784' # yamaguchi
+    Yokkaichi =  's47684' # mie
 
 
 class JmaClient():
